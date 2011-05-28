1d648ba0b70f41fdcaf0b486674b1a590f78089a
thamurath <thamurath@gmail.com>
2011-05-28 15:22:01 +0200

    Trying to add ipython as interpreter.

--- my-python-mode.el
+++ my-python-mode.el
@@ -16,6 +16,8 @@
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+(require 'ipython)
+(add-to-list 'interpreter-mode-alist '("ipython" . python-mode))
 (setq interpreter-mode-alist
       (cons '("python" . python-mode)
 	    interpreter-mode-alist)
