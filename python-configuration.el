9a3467d3c1bd432a0f6618230a9c0f405207f0a4
Jose Luis Ojosnegros Manchon <jose.luis.ojosnegros.manchon@ericsson.com>
2011-11-24 12:26:27 +0100

    New flymake configuration.
    Still python mode is not working properly. I have tryed a lot of things without any result ... It is frustating.

--- python-configuration.el
+++ python-configuration.el
@@ -30,6 +30,20 @@
       )
 )
 
+(require 'flymake)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+                                        ; Make sure it's not a remote buffer or flymake would not work
+    (when (not (subsetp (list (current-buffer)) (tramp-list-remote-buffers)))
+      (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+             (local-file (file-relative-name
+                          temp-file
+                          (file-name-directory buffer-file-name))))
+        (list "pyflakes" (list local-file)))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init))) 
+
 ;;pymacs
 (autoload 'pymacs-apply "pymacs")
 (autoload 'pymacs-call "pymacs")
