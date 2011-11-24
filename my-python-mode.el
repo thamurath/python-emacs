149d2881c4691591310eb60906301ec05dd47c38
Jose Luis Ojosnegros Manchon <jose.luis.ojosnegros.manchon@ericsson.com>
2011-11-24 12:24:47 +0100

    Trying to use ipython as python interpreter

--- my-python-mode.el
+++ my-python-mode.el
@@ -13,6 +13,9 @@
 (add-to-list 'load-path "~/.emacs.d/tim-peters-python-mode")
 (load-library "python")
 
+(require 'ipython)
+(setq py-python-command-args '("--colors" "Linux"))
+
 (autoload 'python-mode "python-mode" "Python Mode." t)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 (add-to-list 'interpreter-mode-alist '("python" . python-mode))
